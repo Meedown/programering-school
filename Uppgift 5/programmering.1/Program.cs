d163f89d0e9ae026d6fea45fa1efd23017b37f9f
unknown <ht24phos@axevalla.fhsk.se>
2024-11-14 10:12:28 +0100

    "school changed"

---
+++ Uppgift 5/programmering.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programmering._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            int saldo = 100;
+            
+
+            while (saldo > 0)
+            {
+
+                Console.WriteLine("your money is " + saldo);
+                Console.WriteLine("what ice cream you want?");
+                Console.WriteLine("1.Piggelin = 10 kr  2.Magnum = 20 kr and 3.Daim = 30 kr");
+                val = int.Parse(Console.ReadLine());
+
+                if (val == 1)
+                {
+
+                    saldo = saldo - 10;
+
+                }
+
+                else if (val == 2)
+                {
+
+                    saldo = saldo - 20;
+
+                }
+
+                else if (val == 3)
+                {
+
+                    saldo = saldo - 30;
+
+                }
+
+ 
+            }
+           
+        }
+    }
+}
