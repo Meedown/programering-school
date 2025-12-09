f4efbac560e70426f349a50440f60d4a290fc586
Philip <ht24phso@axevalla.fhsk.se>
2025-12-09 11:13:37 +0100

    files uploaded

---
+++ Uppgift 03/programering/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age = 0;
+int current_year = 2025;
+int birth_year;
+
+Console.WriteLine("What is your name?");
+string Name = Console.ReadLine();
+Console.WriteLine("What year were you born?");
+birth_year = int.Parse(Console.ReadLine());
+
+age = current_year - birth_year;
+Console.WriteLine("Your name is " + Name + " and you age is " + age);
+Console.ReadLine();
+/*Console.WriteLine("What year were you born in");
+Console.ReadLine();*/
